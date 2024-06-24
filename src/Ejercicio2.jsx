ff6f45059c00bff1777597c6ffc385e38c357024
MatAlumno <135235011+MatAlumno@users.noreply.github.com>
2024-06-24 16:55:18 -0300

    Update Ejercicio2.jsx

--- src/Ejercicio2.jsx
+++ src/Ejercicio2.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 export default function Ejercicio2() {
-  
+  const [formData, setFormData] = useState({
+    names: "",
+    years: 0,
+  });
+  const formChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <div>
       <h2>Ejercicio 2: Formulario</h2>
-      {/* Crear formulario */}
+      <form onSubmit={formChange}>
+        <lavel> Nombre: </lavel>
+        <input type="text"  />
+        <lavel> Years: </lavel>
+        <input type="number"  />
+        <button >ENVIAR</button>
+      </form>ñ
     </div>
-  )
+  );
 }

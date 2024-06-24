5eb6e8be37ef6f35cc99536d9be03eb1e125a675
MatAlumno <135235011+MatAlumno@users.noreply.github.com>
2024-06-24 16:54:29 -0300

    Update Ejercicio1.jsx

--- src/Ejercicio1.jsx
+++ src/Ejercicio1.jsx
@@ -1,12 +1,23 @@
-export default function Ejercicio1() {
+import { useState } from "react"
 
+export default function Ejercicio1() {
+  const [selectData, setSelectData] = useState('');
+  const dataChange = (event) => {
+    setSelectData(event.target.value);
+    alert(event.target.value);
+  };
   return (
     <div>
-      <h2>Ejercicio 1: Lista desplegable</h2>
+      <h1>Ejercicio 1: Lista desplegable</h1>
 
       <label>Seleccione una fruta: </label>
-      {/* Crear lista de selección */}
 
+      <select onChange={dataChange}>
+        <option value="Manzana"> Manzana </option>
+        <option value="Pera"> Pera </option>
+        <option value="Naranja"> Naranja </option>
+        <option value="Mandarina"> Mandarina </option>
+      </select>
     </div>
-  )
+  );
 }
